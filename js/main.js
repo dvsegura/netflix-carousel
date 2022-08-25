0fed33c50bf9d773629c40e4dde7eae65e30274f
Daylin Vazquez <daylinfeina@gmail.com>
2022-08-25 20:31:09 +0200

    Carousel funcional

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const fila = document.querySelector('.contenedor-carousel');
+const peliculas = document.querySelectorAll('.pelicula');
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+
+flechaDerecha.addEventListener("click", () =>{
+    fila.scrollLeft += fila.offsetWidth; 
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+    if(indicadorActivo.nextSibling){
+        indicadorActivo.nextSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+flechaIzquierda.addEventListener("click", () =>{
+    fila.scrollLeft -= fila.offsetWidth; 
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+    if(indicadorActivo.previousSibling){
+        indicadorActivo.previousSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+/***************Paginación*****************/
+const resultadoPagina = 5;
+const numeroPaginas = Math.ceil(peliculas.length/resultadoPagina);
+console.log(numeroPaginas);
+for(let i = 0; i < numeroPaginas; i++){
+    console.log(i);
+    const indicador = document.createElement('button');
+    if(i === 0){
+        indicador.classList.add('activo');
+    }
+    document.querySelector('.indicadores').appendChild(indicador);
+    indicador.addEventListener("click", (e) =>{
+        fila.scrollLeft = i * fila.offsetWidth;
+        document.querySelector('.indicadores .activo').classList.remove('activo');
+        e.target.classList.add('activo'); 
+    });    
+}
+
+/**********/
+peliculas.forEach((pelicula) =>{
+    pelicula.addEventListener("mouseenter", (e) =>{
+        const elemento = e.currentTarget;
+        setTimeout(() => {
+            peliculas.forEach(pelicula =>{
+                pelicula.classList.remove('hover');
+            });
+            elemento.classList.add('hover');
+        },200);
+    });
+});
+fila.addEventListener("mouseleave", () => {
+    peliculas.forEach(pelicula =>{
+        pelicula.classList.remove('hover');
+    });
+});
